5ec21aeff89c2ca50d00521dfe783270d12214e2
Rabeel Fatima <rabeelf43@gmail.com>
2025-11-24 22:41:01 +0500

    submit video 2(input_page)
    "Stage 1: Building the BMI Calculator Input Screen UI"
    
    "Defining the Dark Theme and Scaffold Structure" and input_page.dart file

---
+++ bmi/bmi_app/lib/input_page.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class InputPage extends StatefulWidget {
+  const InputPage({super.key});
+  @override
+  State<InputPage> createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("BMI Calculator"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+          Text(
+          'Body Text Example', // Your text goes here
+          style: TextStyle(
+            fontSize: 24.0,
+            // Setting color explicitly ensures it is visible
+            color: Colors.white,
+          ),
+          ),
+          ],
+
+        ),
+      ),
+    );
+  }
+}
