1d5be66a3a112368ca0413c53ba95607587fc2fd
Rabeel Fatima <rabeelf43@gmail.com>
2025-11-26 11:47:23 +0500

    submit video 5(Refactoring)
    reusable RepeatContainerCode widget to reduce duplicate UI code

--- bmi/bmi_app/lib/input_page.dart
+++ bmi/bmi_app/lib/input_page.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 
+const Color activeCardColor = Color(0xFF1D1E33);
+const Color bottomContainerColor = Color(0xFFEB1555);
+
 class InputPage extends StatefulWidget {
   const InputPage({super.key});
   @override
   State<InputPage> createState() => _InputPageState();
 }
-const Color activeCardColor = Color(0xFF1D1E33);
-const Color bottomContainerColor = Color(0xFFEB1555);
+
 class _InputPageState extends State<InputPage> {
   @override
   Widget build(BuildContext context) {
@@ -21,66 +23,73 @@ class _InputPageState extends State<InputPage> {
             child: Row(
               children: <Widget>[
                 Expanded(
-                  child: Container(
-                    margin: const EdgeInsets.all(10.0),
-                    decoration: BoxDecoration(
-                      color: activeCardColor,
-                      borderRadius: BorderRadius.circular(10.0),
-                    ),
+                  // Corrected the widget usage syntax
+                  child: new RepeatContainerCode(
+                    colors: Color(0xFF1D1E33),
                   ),
                 ),
                 Expanded(
-                  child: Container(
-                    margin: const EdgeInsets.all(10.0),
-                    decoration: BoxDecoration(
-                      color: activeCardColor,
-                      borderRadius: BorderRadius.circular(10.0),
-                    ),
+                  // Corrected the widget usage syntax
+                  child: new RepeatContainerCode(
+                    colors: Color(0xFF1D1E33),
                   ),
                 ),
               ],
             ),
           ),
+
           Expanded(
-            child: Container(
-              margin: const EdgeInsets.all(10.0),
-              decoration: BoxDecoration(
-                color: activeCardColor,
-                borderRadius: BorderRadius.circular(10.0),
-              ),
+            child: new RepeatContainerCode(
+              colors: Color(0xFF1D1E33),
             ),
           ),
+
           Expanded(
-            child: Row(
+            child: new Row(
               children: <Widget>[
                 Expanded(
-                  child: Container(
-                    margin: const EdgeInsets.all(10.0),
-                    decoration: BoxDecoration(
-                      color: activeCardColor,
-                      borderRadius: BorderRadius.circular(10.0),
-                    ),
+                  child: new RepeatContainerCode(
+                    colors: Color(0xFF1D1E33),
                   ),
                 ),
                 Expanded(
-                  child: Container(
-                    margin: const EdgeInsets.all(10.0),
-                    decoration: BoxDecoration(
-                      color: activeCardColor,
-                      borderRadius: BorderRadius.circular(10.0),
-                    ),
+                  child: new RepeatContainerCode(
+                    colors: Color(0xFF1D1E33),
                   ),
                 ),
               ],
             ),
           ),
-          Container(
+//ADDED
+          new Container(
             color: bottomContainerColor,
             margin: const EdgeInsets.only(top: 10.0),
             width: double.infinity,
             height: 60.0,
           ),
         ],
+      ),
+    );
+  }
+}
+//ADDED
+
+// Minimal Refactored Widget
+class RepeatContainerCode extends StatelessWidget {
+  const RepeatContainerCode({
+    Key? key,
+    required this.colors,
+  }) : super(key: key);
+
+  final Color colors;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.all(10.0),
+      decoration: BoxDecoration(
+        color: colors,
+        borderRadius: BorderRadius.circular(10.0),
       ),
     );
   }
